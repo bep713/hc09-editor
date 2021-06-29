4e5e331899c14b8c8c0ea85c013489de0177bacb
bep713 <mpanetta755@gmail.com>
2021-06-29 16:48:37 -0400

    Merging changes to recent file service.

--- src/server/recent-files-service.js
+++ src/server/recent-files-service.js
@@ -2,91 +2,67 @@ const fs = require('fs');
 const path = require('path');
 const { app } = require('deskgap');
 
-const PATH_TO_RECENT_FILES = path.join(app.getPath('userData'), 'recent-files.json');
 const MAX_RECENT_FILES = 10;
 
 let recentFileService = {};
 recentFileService.recentFiles = [];
 
 recentFileService.initialize = () => {
-    if (fs.existsSync(PATH_TO_RECENT_FILES)) {
-        recentFileService.recentFiles = require(PATH_TO_RECENT_FILES);
-        delete require.cache[require.resolve(PATH_TO_RECENT_FILES)];
+  this.PATH_TO_RECENT_FILES = path.join(app.getPath('userData'), 'recent-files.json');
 
-        if (recentFileService.recentFiles instanceof Array) {
-            recentFileService.recentFiles = {
-                'recentASTFiles': recentFileService.recentFiles
-            };
-
-            writeToRecentFilesStore(recentFileService.recentFiles);
-        }
-    }
-    else {
-        recentFileService.recentFiles = {};
-    }
+  if (fs.existsSync(this.PATH_TO_RECENT_FILES)) {
+    recentFileService.recentFiles = require(this.PATH_TO_RECENT_FILES);
+    delete require.cache[require.resolve(this.PATH_TO_RECENT_FILES)]
+  }
+  else {
+    recentFileService.recentFiles = [];
+  }
 };
 
-recentFileService.addFile = (key, options) => {
-    let recentFile = {
-        'path': options.path,
-        'type': options.type,
-        'time': options.time ? options.time : Date.now()
-    };
+recentFileService.addFile = (filePath, type) => {
+  const indexInRecents = recentFileService.recentFiles.findIndex((file) => {
+    return file.path === filePath;
+  });
 
-    if (!recentFileService.recentFiles[key]) {
-        recentFileService.recentFiles[key] = [recentFile];
-    }
-    else {
-        const nodeDoesNotExist = recentFileService.recentFiles[key].find((file) => {
-            return file.path.toLowerCase() === recentFile.path.toLowerCase();
-        }) === undefined;
+  if (indexInRecents >= 0) {
+    recentFileService.recentFiles[indexInRecents].time = Date.now();
+  }
+  else {
+    recentFileService.recentFiles.push({
+      'path': filePath,
+      'type': type,
+      'time': Date.now()
+    });
 
-        if (nodeDoesNotExist) {
-            recentFileService.recentFiles[key].push(recentFile);
-    
-            if (recentFileService.recentFiles[key].length > MAX_RECENT_FILES) {
-                recentFileService.recentFiles[key] = recentFileService.recentFiles[key].slice(MAX_RECENT_FILES * -1);
-            }
-        }
+    if (recentFileService.recentFiles.length > MAX_RECENT_FILES) {
+      recentFileService.recentFiles = recentFileService.getRecentFiles().slice(0, MAX_RECENT_FILES);
     }
-    
-    writeToRecentFilesStore(recentFileService.recentFiles);
+  }
+
+  recentFileService._writeToRecentFilesStore(recentFileService.recentFiles);
 };
 
-recentFileService.removeFile = (key, path) => {
-    const recentFilesInCategory = recentFileService.recentFiles[key];
+recentFileService.removeFile = (filePath) => {
+  const indexInRecents = recentFileService.recentFiles.findIndex((file) => {
+    return file.path === filePath;
+  });
 
-    if (recentFilesInCategory) {
-        const nodeIndex = recentFilesInCategory.findIndex((file) => {
-            return file.path.toLowerCase() === path.toLowerCase();
-        });
-
-        if (nodeIndex >= 0) {
-            recentFilesInCategory.splice(nodeIndex, 1);
-        }
-
-        if (recentFilesInCategory.length === 0) {
-            delete recentFileService.recentFiles[key];
-        }
-
-        writeToRecentFilesStore(recentFileService.recentFiles);
-    }
+  if (indexInRecents >= 0) {
+    recentFileService.recentFiles.splice(indexInRecents, 1);
+    recentFileService._writeToRecentFilesStore(recentFileService.recentFiles);
+  }
 };
 
 recentFileService.getRecentFiles = () => {
-    return recentFileService.recentFiles.sort((a, b) => { return b.time - a.time; });
+  return recentFileService.recentFiles.sort((a, b) => { return b.time - a.time; });
 };
 
-recentFileService.getRecentFilesByCategory = (category) => {
-    return recentFileService.recentFiles[category];
+recentFileService._writeToRecentFilesStore = (data) => {
+  fs.writeFile(this.PATH_TO_RECENT_FILES, JSON.stringify(data), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
 };
 
 module.exports = recentFileService;
-
-function writeToRecentFilesStore(data) {
-    fs.writeFile(PATH_TO_RECENT_FILES, JSON.stringify(data), function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-};
