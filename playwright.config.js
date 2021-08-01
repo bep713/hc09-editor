7f7b6678577961c0e5983b4935ba5b2a8a168338
bep713 <mpanetta755@gmail.com>
2021-08-01 16:10:31 -0400

    Cleaning playwright config.

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,5 @@
 const config = {
-    testDir: 'test/ui/e2e',
-
-    // use: {
-    //     launchOptions: {
-    //         executablePath: 'lib/deskgap/dist/Deskgap/HC09Editor.exe'
-    //     }
-    // },
+    testDir: 'test/ui/e2e'
 }
 
 module.exports = config;
